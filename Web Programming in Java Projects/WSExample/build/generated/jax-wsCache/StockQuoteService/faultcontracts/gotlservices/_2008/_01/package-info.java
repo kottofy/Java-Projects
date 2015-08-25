f868c5be1464e5e50e7594dd44e9dfb6b6b66b30
kottofy <kottofy@gmail.com>
2015-08-25 15:21:16 -0400

    removed a project

--- Web Programming in Java Projects/WSExample/build/generated/jax-wsCache/StockQuoteService/faultcontracts/gotlservices/_2008/_01/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://GOTLServices.FaultContracts/2008/01", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package faultcontracts.gotlservices._2008._01;
